ad495ac4cbcb51250c794fcbfa02c9b803f0f738
Rubén <rubenalb90@gmail.com>
2020-09-12 00:07:30 +0200

    .gitignore updated

--- .docz/.cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/003f4bb1179bd0afc0bc921edc8b96bc.js
+++
@@ -1,9 +0,0 @@
-import MUIEditor, { MUIEditorState } from "..\\..\\..\\..\\..\\src";
-import { Playground } from "docz";
-import * as React from 'react';
-export default {
-  MUIEditor,
-  MUIEditorState,
-  Playground,
-  React
-};
